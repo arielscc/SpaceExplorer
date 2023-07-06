9a790acf66f07b70355a94e315ea077a29ffee9b
Ariel Chura <ariel.chura.c@gmail.com>
2023-07-06 00:32:25 -0400

    feat: update navigation icons

---
+++ app/assets/navigation/index.ts
@@ -0,0 +1,7 @@
+import EventsIcon from './calendar.svg';
+import GalleryIcon from './gallery.svg';
+import HomeIcon from './head.svg';
+import SpaceIcon from './rocket.svg';
+import WeatherIcon from './weather.svg';
+
+export {EventsIcon, GalleryIcon, HomeIcon, SpaceIcon, WeatherIcon};
