735644dac12264c6a92c8f1ed0663f12fbd8e9bf
Ariel Chura <ariel.chura.c@gmail.com>
2023-07-10 23:43:58 -0400

    feat: add margin insets

--- app/screens/Events.tsx
+++ app/screens/Events.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import WorldEvents from '../components/WorldEvents';
 
 const Events = () => {
+  const {top} = useSafeAreaInsets();
   return (
-    <ScrollView>
+    <ScrollView style={{marginTop: top}}>
       <WorldEvents />
     </ScrollView>
   );
--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import DailyImage from '../components/DailyImage';
 
 const Home = () => {
+  const {top} = useSafeAreaInsets();
   return (
-    <ScrollView>
+    <ScrollView style={{marginTop: top}}>
       <DailyImage />
     </ScrollView>
   );
